94049353217fcb1ae31f174db429c7b79df3e559
ermiry <erick.salas@ermiry.com>
2021-08-21 20:52:37 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define BYND_VERSION			"0.1"
 #define BYND_VERSION_NAME		"Version 0.1"
-#define BYND_VERSION_DATE		"17/07/2021"
-#define BYND_VERSION_TIME		"17:03 CST"
+#define BYND_VERSION_DATE		"21/08/2021"
+#define BYND_VERSION_TIME		"20:52 CST"
 #define BYND_VERSION_AUTHOR		"Erick Salas"
 
 // print full bynd version information
